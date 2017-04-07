26cfcce2dce0bca2b7b84889901f9fd9b421b6f9
Nonoo <nonoo@nonoo.hu>
2017-04-07 11:07:33 +0000

    Add missing config parameters

--- config-example.inc.php
+++ config-example.inc.php
@@ -8,6 +8,8 @@
 	define('APRS_SERVER',					'hun.aprs2.net');
 	define('APRS_SERVER_PORT',				14580);
 	define('APRS_DEFAULT_TEXT',				'Brandmeister DMR');
+	define('APRS_SYMBOL_REPEATER',			'/r');
+	define('APRS_SYMBOL_SIMPLEX_STATION',	'/Q');
 
 	// MySQL user, password, host, Registry database name and table names.
 	define('DMR_DB_USER',					'ham-dmr.hu');
